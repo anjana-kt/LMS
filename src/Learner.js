331cfa36ce06beee9dfb739ce649317f13a4c333
Anjana <anjanakta@gmail.com>
2023-01-30 22:39:06 +0530

    update

--- src/Learner.js
+++ src/Learner.js
@@ -8,7 +8,7 @@ const Learner = ()=>{
 
     return(
         <>
-            <h1>VWelcome</h1>
+            <h1>Welcome</h1>
         </>
     );
 }
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import './styles/index.css';
+import Home from './Home';
+import Learner from './Learner.js'
+import Org from './Org.js'
+document.title="LMS";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<Home />}/>
+          <Route path="learner" element={<Learner />} />
+          <Route path="org" element={<Org />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
